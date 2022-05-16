2b1956ec92db85ec33d8a5f0a965818d0b73e01b
Kevin Reid <kevinnivek@me.com>
2022-05-16 16:51:23 -0400

    added prompt asking for number of characters

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,38 @@
 // Assignment code here
 
+// Asking for input for password length here
+var getPasswordLength = function() {
+  var passwordlength = "";
+  while (passwordlength === "" || passwordlength === null) {
+      passwordlength = prompt (" Let me know how many characters should be in your password, choose a number between 8 and 128");
+  }
+
+  // pazrsing password value into integer
+  passwordlength = parseInt(passwordlength);
+
+  if (passwordlength < 8 ) {
+     window.alert("Sorry, but your password must be at least 8 characters in length. Please input again.")
+     console.log("original number choice was " + passwordlength);
+     getPasswordLength();
+  }
+  else{
+  console.log("You have chosen a password length of " + passwordlength);
+  return passwordlength;
+  }
+};
+
+
+// password values here
+var password ={
+    length: getPasswordLength(),
+    uppercase: false,
+    lowercase: true,
+    numeric: true,
+    specialCharacter: true,
+
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -9,7 +42,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
